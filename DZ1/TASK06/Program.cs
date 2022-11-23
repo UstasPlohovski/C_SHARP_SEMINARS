a83ab0b9ac244cb9e09dd13a2c461bb864d0a4c6
Ustas <ustasplohovski@gmail.com>
2022-11-23 17:22:41 +0300

    Добавлено решение задачи в TASK06

--- DZ1/TASK06/Program.cs
+++ DZ1/TASK06/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6 - определение четности или нечетности введенного числа.
+
+Console.Write("Enter the any integer and press Enter:");
+
+double number = Convert.ToInt32(Console.ReadLine());
+
+double result = number % 2;
+
+if (result == 0)
+{
+    Console.WriteLine($"Nuber {number} is even number");
+}
+else
+{
+
+    Console.WriteLine($"Nuber {number} is not even number");
+}
