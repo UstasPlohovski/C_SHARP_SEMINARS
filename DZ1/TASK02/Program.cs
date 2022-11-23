07dd06d3bd541d484e7504abbdea7263ffe3e9d0
Ustas <ustasplohovski@gmail.com>
2022-11-23 11:00:17 +0300

    Добавлено решение задачи в TASK02

--- DZ1/TASK02/Program.cs
+++ DZ1/TASK02/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2 - из двух чисел найти минимальное и максимальное
+
+Console.Write("Enter the first any integer:");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second any integer:");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"The first number {number1} is MAX");
+    Console.WriteLine($"The second number {number2} is MIN");
+}
+else
+{
+    Console.WriteLine($"The second number {number2} is MAX");
+    Console.WriteLine($"The first number {number1} is MIN");
+}
