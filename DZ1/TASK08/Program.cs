b875598da910e0486ddd163a094d4437743179d7
Ustas <ustasplohovski@gmail.com>
2022-11-24 17:50:29 +0300

    Добавлено решение задачи TASK08

--- DZ1/TASK08/Program.cs
+++ DZ1/TASK08/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// Задача - вывести все четные числа от 1 до N
+
+Console.WriteLine("Enter number");
+int Number = Convert.ToInt32(Console.ReadLine());
+int CurrentNumber = 1;
+
+while (CurrentNumber <= Number)
+
+{
+    if (CurrentNumber%2 == 0)
+    { 
+        Console.Write($"{CurrentNumber}, ");
+    }
+        CurrentNumber++ ;
+}
