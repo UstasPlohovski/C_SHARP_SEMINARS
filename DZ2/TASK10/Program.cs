a3dffb5d5ef7a35e00739036ad41af980664726f
Ustas <ustasplohovski@gmail.com>
2022-11-25 16:39:17 +0300

    Добавлено решение задачи TASK10

---
+++ DZ2/TASK10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача - определить вторую цифру в любом произвольном трехзначном заданном целом числе.
+
+Console.WriteLine("Enter number from 1 to 999");
+int Number = Convert.ToInt32(Console.ReadLine());
+int Remainder = 0;
+
+if (Number > 0 && Number < 1000)
+{
+    Number /= 10;
+    Remainder = Number % 10;
+
+    Console.WriteLine($"The third number from the left is {Remainder}");
+
+}
+else
+{
+    Console.WriteLine("You enter the number is not from 0 to 1000");
+}
