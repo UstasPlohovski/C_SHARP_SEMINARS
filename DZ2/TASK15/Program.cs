5dc02c7b89bdf96597e1c39cd95d3ba4b835c376
Ustas <ustasplohovski@gmail.com>
2022-11-29 18:00:31 +0300

    Добавлено решение задачи TASK15

---
+++ DZ2/TASK15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача - опредедить выходной или рабочий введенный день недели
+
+Console.WriteLine("Enter your number - day of the week");
+int NumberDay = Convert.ToInt32(Console.ReadLine());
+
+if (NumberDay == 6 || NumberDay ==7 )
+{
+Console.WriteLine("This day is wekkend");
+}
+else
+{
+Console.WriteLine("This day is not wekkend");
+}
