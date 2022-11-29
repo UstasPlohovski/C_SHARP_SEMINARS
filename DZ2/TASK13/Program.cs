6bd604095321e075349857ae01ee1d8a66054196
Ustas <ustasplohovski@gmail.com>
2022-11-29 17:28:55 +0300

    Добавлено решение задачи TASK13

---
+++ DZ2/TASK13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача - определить третью цифру слева в любом произвольном заданном целом числе.
+//    (решение через цикл)
+
+Console.WriteLine("Enter your number");
+int Number = Convert.ToInt32(Console.ReadLine());
+int CurrentNumber = Number, Remainder = 0;
+
+if (Number > 99)
+{
+    if (CurrentNumber > 1000)
+    {
+    while (CurrentNumber > 1000)
+    {
+        CurrentNumber /= 10;
+        Remainder = CurrentNumber % 10;
+    }
+    Console.WriteLine($"The third number from the left is {Remainder}");
+    }
+    else
+    Remainder = CurrentNumber % 10;
+    Console.WriteLine($"The third number from the left is {Remainder}");
+}
+else
+{
+Console.WriteLine("The thidr number isn't in the number");
+}
