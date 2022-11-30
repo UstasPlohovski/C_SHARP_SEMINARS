e26bcefd30673a511644e6c660de206ac0df7573
Ustas <ustasplohovski@gmail.com>
2022-12-01 01:09:57 +0300

    Исправлены опечатки в задаче TASK10

--- DZ2/TASK10/Program.cs
+++ DZ2/TASK10/Program.cs
@@ -1,18 +1,18 @@
 ﻿// Задача - определить вторую цифру в любом произвольном трехзначном заданном целом числе.
 
-Console.WriteLine("Enter number from 1 to 999");
+Console.WriteLine("Enter number from 100 to 999");
 int Number = Convert.ToInt32(Console.ReadLine());
 int Remainder = 0;
 
-if (Number > 0 && Number < 1000)
+if (Number >= 100 && Number < 1000)
 {
     Number /= 10;
     Remainder = Number % 10;
 
-    Console.WriteLine($"The third number from the left is {Remainder}");
+    Console.WriteLine($"The second number from the left is {Remainder}");
 
 }
 else
 {
-    Console.WriteLine("You enter the number is not from 0 to 1000");
+    Console.WriteLine("You entered the number is not from 100 to 999");
 }
