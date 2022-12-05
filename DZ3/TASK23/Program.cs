798568981edb98e9238b2e973b2aaf5641d03a5b
Ustas <ustasplohovski@gmail.com>
2022-12-05 22:47:25 +0300

    Добавлены комментарии к задаче TASK23

--- DZ3/TASK23/Program.cs
+++ DZ3/TASK23/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+try                                                                     // основной блок алгоритма
+{
+    Console.WriteLine("Введите любое положительное целое число:");
+    int Number = Convert.ToInt32(Console.ReadLine());
+
+    for (int i=1; i<=Number; i++)
+    {
+        Console.Write($"Куб числа {i} = ");
+        Console.WriteLine(Kube(i));
+    }
+
+}
+catch                                                                   // блок алгоритма для ошибок
+{
+    Console.WriteLine("Введенное число не корректно");
+}
+
+// Создание функции возвратного типа
+int Kube (int n)
+{
+    return n*n*n;
+}
