555f1e2452f057d1dd52acdc1b1b489ce030f36e
Ustas <ustasplohovski@gmail.com>
2022-12-31 00:30:26 +0300

    Добавлено решение задач ДЗ 9.

---
+++ DZ9/TASK64/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в 
+// промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+PrintNumbersWhile(N);
+Console.WriteLine();
+Console.WriteLine(PrintNumbersRec(N));
+
+void PrintNumbersWhile(int N)
+{
+   while (true)
+   {
+     Console.Write($"{N}, ");
+     N--;
+     if (N==0) break;
+   }
+}
+
+string PrintNumbersRec(int N)
+{
+   string Temp = " ";
+   if (N!=0)
+   {
+    Temp = N + ", " + PrintNumbersRec(N-1);
+   } 
+   return Temp;
+}
---
+++ DZ9/TASK66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных
+// элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите натуральное число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма элементов от {M} до {N} = {SumNumbers(M, N)}");
+
+int SumNumbers(int M, int N)
+{
+    if (M == N) return N;
+    return N + SumNumbers(M, N - 1);
+}
